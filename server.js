1de372c09ceb051be6ea5ad5754900af50bdf470
Precious <adaezedawen107@gmail.com>
2024-12-13 18:00:02 +0100

    initial push

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ passport.serializeUser((user, done) => {
 });
 passport.deserializeUser((id, done) => {
   myDB.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
+    done(doc);
   });
 });
 
